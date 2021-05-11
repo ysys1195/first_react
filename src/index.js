64afe34773a8dc593d15d9694c8c2e4177102ff8
ysys1195 <73346157+ysys1195@users.noreply.github.com>
2021-05-11 22:18:36 +0900

    Square constructorのstate初期化

--- src/index.js
+++ src/index.js
@@ -3,9 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
+  
   render() {
     return (
-      <button className="square">
+      <button className="square" onClick={() => alert('hey')}>
         {this.props.value}
       </button>
     );
