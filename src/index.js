c7cd6a83c0d331e7d3bfadd5019c43f509213b76
ysys1195 <73346157+ysys1195@users.noreply.github.com>
2021-05-12 22:50:58 +0900

    9個のマスにnull値セット

--- src/index.js
+++ src/index.js
@@ -26,8 +26,16 @@ class Square extends React.Component {
 }
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    // ９個のマス目それぞれにnull値をセットする
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
+
   renderSquare(i) {
-    return <Square　value={i} />;
+    return <Square value={i} />;
   }
 
   render() {
